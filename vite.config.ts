df5a79fd361e0ebe06787743ba40ffeff2bbd97d
周星星同学 <mrcxt@foxmail.com>
2024-01-04 16:54:33 +0800

    refactor: ♻️ 部分内容调整

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import mdx from "@mdx-js/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 9527,
+  },
   plugins: [
     { enforce: "pre", ...mdx(/* jsxImportSource: …, otherOptions… */) },
     react(),
